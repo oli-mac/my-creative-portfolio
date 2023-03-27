22badd0f32f769ba5940e0168a46ed7c2340763a
oli-mac <adumulugeta21@gmail.com>
2023-03-28 01:10:34 +0300

    setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,txs}",
+    "./components/**/*.{js,ts,jsx,txs}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
